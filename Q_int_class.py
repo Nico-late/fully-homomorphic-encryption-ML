2fa226a808e914ea68c6fa30f14d677b08cb09c8
nico-late <nicolasscholten@live.fr>
2022-03-21 22:30:53 +0100

    apply_operations input simplified

--- Q_int_class.py
+++ Q_int_class.py
@@ -1,15 +1,27 @@
 from mlp_integers import MLP
 import  os.path
 from create_datasets import create_dataset_Q_int
+import numpy as np
 
 class Q_int_class(object):
     def __init__(self, Q):
         self.mlp = {}
         self.operations=['AND','XOR','OR']
+
+        # Create weights directory if doesn't exist
+        if not os.path.exists('mlp_weights'):
+            os.mkdir('mlp_weights')
+
         for operation in self.operations:
+
+            # Create operations sub-directories if don't exist
+            if not os.path.exists('mlp_weights/{}'.format(operation)):
+                os.mkdir('mlp_weights/{}'.format(operation))
+
+            # Load or create MLP model for each operation
             self.mlp[operation] = MLP(layers=[2, 10, 2], activations=['relu', 'linear'], Q=Q)
             path = 'mlp_weights/{}/mlp_weights_Q={}_2_10_2.model'.format(operation,Q)
-            if os.path.isfile(path):
+            if os.path.exists(path):
                 self.mlp[operation].load_weights(path)
             else:
                 print('##### Model does not exist for operation {} #####'.format(operation))
@@ -18,5 +30,9 @@ class Q_int_class(object):
                 self.mlp[operation].train(tr_x, tr_y, val_x, val_y, epochs=100, batch_size=32, lr=100, decay_steps=10)
                 self.mlp[operation].save_weights(path)
 
-    def apply_operations(self, X, operation):
+    # Function that returns the selected operation between the values in X
+    def apply_operations(self, X_1, X_2, operation):
+        X = np.array([(np.array([x_1, x_2]).reshape(2, 1)) for x_1, x_2 in zip(X_1, X_2)])
+        if operation not in self.operations:
+            raise ValueError("Avalailable operations are {}".format(self.operations))
         return self.mlp[operation].predict(X)
--- main.py
+++ main.py
@@ -3,17 +3,15 @@ import numpy as np
 
 Q = 2**20
 
-#X_1 = np.random.randint(2, size=2)
-#X_2 = np.random.randint(2, size=2)
-X_1 = np.array([[0],[1],[0],[1]])
-X_2 = np.array([[0],[0],[1],[1]])
+#X_1 = Q * np.random.randint(2, size=2)
+#X_2 = Q * np.random.randint(2, size=2)
+X_1 = Q * np.array([0,1,0,1])
+X_2 = Q * np.array([0,0,1,1])
 
-data = Q * np.array([(np.array([x_1, x_2]).reshape(2, 1)) for x_1, x_2 in zip(X_1, X_2)])
 test = Q_int_class(Q)
 
-for i in range(data.shape[0]):
-    print("Data n°{}".format(i))
-    print('Input {}'.format(list(data[i])))
-    print('OR Result : {}'.format(test.apply_operations(data,'OR')[i][0][0]))
-    print('AND Result : {}'.format(test.apply_operations(data,'AND')[i][0][0]))
-    print('XOR Result : {}\n'.format(test.apply_operations(data,'XOR')[i][0][0]))
+for i in range(X_1.shape[0]):
+    print('Input : {} and {}'.format(X_1[i],X_2[i]))
+    print('OR Result : {}'.format(test.apply_operations(X_1,X_2,'OR')[i][0][0]))
+    print('AND Result : {}'.format(test.apply_operations(X_1,X_2,'AND')[i][0][0]))
+    print('XOR Result : {}\n'.format(test.apply_operations(X_1,X_2,'XOR')[i][0][0]))
